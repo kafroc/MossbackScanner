5446feaa0b6dddb9ca20e7b6d2c36ba8907ae153
Kafroc <kafrocyang@gmail.com>
2020-09-23 18:48:58 +0800

    Add files via upload

---
+++ mossbackScaner.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+# version: 0.2
+# date: 2020.09.22
+
+import json
+import re
+import socket
+import sys
+import http.client
+import time
+import threading
+
+from socket import *
+from time import ctime
+import hashlib
+import subprocess
+from subprocess import PIPE
+from scanner_sqli import test_sqli
+
+global glo_conf
+global glo_pkg_list
+global glo_lock
+global glo_scanner
+
+def scanner_factory(name):
+    return eval(name + "()")
+
+with open('config.json', 'r') as fp:
+    glo_conf = json.loads(fp.read())
+
+glo_pkg_list = []
+
+glo_lock = threading.Lock()
+
+glo_scanner = []
+glo_scanner.append(scanner_factory("test_sqli"))
+
+def do_scan_thread():
+    global glo_pkg_list
+    global glo_lock
+    global glo_scanner
+
+    while True:
+        glo_lock.acquire()
+        if len(glo_pkg_list) > 0:
+            pkg = json.loads(glo_pkg_list.pop(0))
+            glo_lock.release()
+            
+            # do all test here
+            for fun in glo_scanner:
+                fun.run(pkg['method'], pkg['uri'], pkg['version'], pkg['header'], pkg['body'], pkg['host'])
+            # testing finished
+        else:
+            time.sleep(1)
+            glo_lock.release()
+
+def main():
+    global glo_pkg_list
+    global glo_lock
+    global glo_conf
+    
+    t = threading.Thread(target=do_scan_thread, args=())
+    t.start()
+    
+    BUFSIZ = 1024
+    ADDRESS = ('', glo_conf['scanner_port'])
+    udpServerSocket = socket(AF_INET, SOCK_DGRAM)
+    udpServerSocket.bind(ADDRESS)
+
+    while True:
+        # pack_meta = pack_len(4 B) + pack_hash(32 B)
+        pack_meta, pack_from = udpServerSocket.recvfrom(36)
+        pack_meta = pack_meta.decode()
+        pack_data = ''
+        for i in range(int(pack_meta[:4])):
+            data, _ = udpServerSocket.recvfrom(BUFSIZ)
+            pack_data += data.decode()
+
+        m = hashlib.md5()
+        m.update(pack_data.encode())
+
+        if pack_meta[4:] != m.hexdigest():
+            print("[ERROR] the hash of received message incorrect.")
+            udpServerSocket.sendto("ER".encode('utf-8'), pack_from)
+        else:
+            udpServerSocket.sendto("OK".encode('utf-8'), pack_from)
+
+        glo_lock.acquire()
+        glo_pkg_list.append(pack_data)
+        glo_lock.release()
+
+    udpServerSocket.close()
+
+if __name__ == "__main__":
+    main()
---
+++ scanner_sqli.py
@@ -0,0 +1,88 @@
+import json
+import time
+import http.client
+
+class test_sqli():
+    def __init__(self):
+        self.filecnt = 1
+        with open('config.json', 'r') as fp:
+            self.conf = json.loads(fp.read())
+    
+    def format_save(self, method, uri, version, header, body):
+        first_line = method + ' ' + uri + ' ' + version + '\n'
+        hj = json.loads(header)
+        for head in hj:
+            first_line += head + ': ' + hj[head] + '\n'
+        first_line += '\n'
+        if method == 'POST':
+            first_line += body
+
+        fn = self.conf['scanner_path'] + "logs/" + time.strftime("%Y-%m-%d_%H_%M_%S", time.localtime()) + '_SQLi.txt'
+        with open(fn, 'w') as fp:
+            fp.write(first_line)
+        self.filecnt += 1
+    
+    def test_sqli_uri(self, method, uri, version, header, body, host):
+        with open(self.conf['scanner_path'] + self.conf['payload_file'], "r") as fp:
+            payloads = fp.readlines()
+
+        params = uri.split('?')
+        if len(params) == 2:
+            path = params[0] + '?'
+            params = params[1].split('&')
+
+            for i in range(len(params)):
+                param_bak = params[i]
+                for payload in payloads:
+                    time.sleep(0.001 * self.conf["interval"])
+                    params[i] = param_bak + payload.strip()
+                    #print(params[i])
+                    uri_new = '&'.join(params)
+                    # print(uri_new)
+                    hc = http.client.HTTPConnection(host, timeout=3)
+                    hc.request(method, path+uri_new.replace(' ', '%20'), body, json.loads(header))
+                    try:
+                        hc.getresponse().read()
+                    except Exception as exp:
+                        print("=========== FIND VUL. =============")
+                        print(uri_new.replace(' ', '%20'))
+                        self.format_save(method, path+uri_new.replace(' ', '%20'), version, header, body)
+                        # break 
+                    params[i] = param_bak
+
+    def test_sqli_body(self, method, uri, version, header, body, host):
+        with open(self.conf['scanner_path'] + "payloads\\poc_sqlserver.txt", "r") as fp:
+            payloads = fp.readlines()
+
+        if method != 'POST':
+            return
+        
+        bodys = body.split('&')
+        for i in range(len(bodys)):
+            body_bak = bodys[i]
+            if 'ContentPlaceHolder1%24code=' not in body_bak:
+                continue
+            for payload in payloads:
+                time.sleep(0.001 * self.conf["interval"])
+                bodys[i] = body_bak + payload.strip()
+                body_new = '&'.join(bodys)
+                # print(body_new)
+                hc = http.client.HTTPConnection(host, timeout=3)
+
+                hj = json.loads(header)
+                hj['Content-Length'] = len(body_new.replace(' ', '%20'))
+                hc.request(method, uri, body_new.replace(' ', '%20'), hj)
+                try:
+                    hc.getresponse().read()
+                except Exception as exp:
+                    print("=========== FIND VUL. =============")
+                    print(body_new.replace(' ', '%20'))
+                    self.format_save(method, uri, version, header, body_new.replace(' ', '%20'))
+                    # break
+                bodys[i] = body_bak
+                    
+    def run(self, method, uri, version, header, body, host):
+        print('Doing SQL injection: ' + uri)
+        self.test_sqli_uri(method, uri, version, header, body, host)
+        if method == 'POST':
+            self.test_sqli_body(method, uri, version, header, body, host)
