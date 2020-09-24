7ab003f59cd47f75335f0cf2739b8f155bbc53b2
Kafroc <kafrocyang@gmail.com>
2020-09-24 20:52:39 +0800

    Update scanner_unauth_access.py

--- scanner_unauth_access.py
+++ scanner_unauth_access.py
@@ -1,30 +1,16 @@
 import json
 import time
 import http.client
+from utils import format_save
+
 
 class test_unauth_access():
     def __init__(self):
         self.filecnt = 1
         with open('config.json', 'r') as fp:
             self.conf = json.loads(fp.read())
-    
-    def format_save(self, method, uri, version, header, body):
-        print("**************************************************")
-        print("Find unauth access")
-        
-        first_line = method + ' ' + uri + ' ' + version + '\n'
-        hj = json.loads(header)
-        for head in hj:
-            first_line += head + ': ' + hj[head] + '\n'
-        first_line += '\n'
-        if method == 'POST':
-            first_line += body
+        self.log = format_save('unauth')
 
-        fn = self.conf['scanner_path'] + "logs/" + time.strftime("%Y-%m-%d_%H_%M_%S", time.localtime()) + '_unauthAccess.txt'
-        with open(fn, 'w') as fp:
-            fp.write(first_line)
-        self.filecnt += 1
-    
     def test_req_with_cookie(self, method, uri, version, header, body, host):
         hc = http.client.HTTPConnection(host, timeout=3)
         hc.request(method, uri, body, json.loads(header))
@@ -38,12 +24,16 @@ class test_unauth_access():
         hc.request(method, uri, body, hj)
         l2 = hc.getresponse().getheader("Content-Length")
         return l2
-                    
+
     def run(self, method, uri, version, header, body, host):
-        if uri.split('?')[0].split('.')[-1] in ['asp']:
-            l1 = self.test_req_with_cookie(method, uri, version, header, body, host)
-            l2 = self.test_req_without_cookie(method, uri, version, header, body, host)
+        print('Doing unauth access: ' + uri)
+        if uri.split('?')[0].split('.')[-1] not in ['html', 'htm', 'shtml', 'js', 'css', 'jpeg', 'jpg', 'png', 'gif']:
+            l1 = self.test_req_with_cookie(
+                method, uri, version, header, body, host)
+            l2 = self.test_req_without_cookie(
+                method, uri, version, header, body, host)
+
             if int(l1) == int(l2):
-                self.format_save(method, uri, version, header, body)
+                self.log.format_save(method, uri, version, header, body)
             else:
                 print(int(l1) - int(l2))
