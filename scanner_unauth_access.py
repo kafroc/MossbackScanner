21f5fe3018da381a295b62cf28477baaf2d5b05f
Kafroc <kafrocyang@gmail.com>
2020-11-06 16:21:02 +0800

    Update scanner_unauth_access.py

--- scanner_unauth_access.py
+++ scanner_unauth_access.py
@@ -14,35 +14,66 @@ class test_unauth_access():
         self.name = 'unauth'
         self.log = format_save(self.name)
         self.httptimeout = 10
+        self.http_client = None
         self.checkpkg = check_repeat_package(key_with_value=True)
 
-    def test_req_with_cookie(self, method, uri, version, header, body, host):
-        if self.conf['https_server'] is True:
-            hc = http.client.HTTPSConnection(host, timeout=self.httptimeout)
-        else:
-            hc = http.client.HTTPConnection(host, timeout=self.httptimeout)
-        hc.request(method, uri, body, json.loads(header))
-        l1 = hc.getresponse().getheader("Content-Length")
+    def send_recv(self, method, uri, version, header, body, host):
+        if self.http_client == None:
+            srv = host.split(':')
+            srvhost = srv[0]
+            if len(srv) != 2:
+                if self.conf['https_server'] is True:
+                    srvport = 443
+                else:
+                    srvport = 80
+            else:
+                srvport = srv[1]
 
-        if l1 is None:
-            return 0
+            try:
+                if self.conf['proxy_forward'] != '':
+                    pxy = self.conf['proxy_forward'].split(':')
+                    pxyhost = pxy[0]
+                    pxyport = pxy[1]
 
-        return int(l1)
+                    if self.conf['https_server'] is True:
+                        self.http_client = http.client.HTTPSConnection(
+                            pxyhost, pxyport, timeout=self.httptimeout)
+                    else:
+                        self.http_client = http.client.HTTPConnection(
+                            pxyhost, pxyport, timeout=self.httptimeout)
 
-    def test_req_without_cookie(self, method, uri, version, header, body, host):
-        if self.conf['https_server'] is True:
-            hc = http.client.HTTPSConnection(host, timeout=self.httptimeout)
-        else:
-            hc = http.client.HTTPConnection(host, timeout=self.httptimeout)
-        hj = json.loads(header)
-        hj["Cookie"] = ''
-        hc.request(method, uri, body, hj)
-        l2 = hc.getresponse().getheader("Content-Length")
+                    self.http_client.set_tunnel(srvhost, srvport)
+                else:
+                    if self.conf['https_server'] is True:
+                        self.http_client = http.client.HTTPSConnection(
+                            srvhost, srvport, timeout=self.httptimeout)
+                    else:
+                        self.http_client = http.client.HTTPConnection(
+                            srvhost, srvport, timeout=self.httptimeout)
+            except Exception as exp:
+                printDarkRed("[ERROR] [UNAUTH] http.client.connection")
+                printDarkRed(exp)
+                return -1
 
-        if l2 is None:
+        headjson = json.loads(header)
+        headjson['Content-Length'] = len(body)
+        try:
+            self.http_client.request(
+                method, uri.replace(' ', '%20'), body, headjson)
+        except Exception as exp:
+            printDarkRed("[ERROR] [UNAUTH] http.client.request")
+            printDarkRed(exp)
+            self.http_client = None
             return -1
 
-        return int(l2)
+        try:
+            ret = self.http_client.getresponse().getheader("Content-Length")
+            return int(ret)
+        except Exception as exp:
+            printDarkRed("[ERROR] [UNAUTH] http.client.response")
+            printDarkRed(exp)
+            self.http_client = None
+            return -1
 
     def run(self, method, uri, version, header, body, host):
         if self.checkpkg.is_repeat_pkg(method, uri, body) is True:
@@ -52,15 +83,21 @@ class test_unauth_access():
             return
 
         if uri.split('?')[0].split('.')[-1] not in self.conf['static']:
-            printGreen('Doing %s testing: %s' % (self.name, uri))
+            printGreen('Doing %s testing: %s %s/%s' %
+                       (self.name, method, host, uri))
 
-            l1 = self.test_req_with_cookie(
+            l1 = self.send_recv(
                 method, uri, version, header, body, host)
-            l2 = self.test_req_without_cookie(
+
+            header_new = json.loads(header)
+            header_new["Cookie"] = ''
+            header = json.dumps(header_new)
+            l2 = self.send_recv(
                 method, uri, version, header, body, host)
 
             try:
-                if l1 == l2:
+                if l1 != -1 and l2 != -1 and l1 == l2:
                     self.log.format_save(method, uri, version, header, body)
             except Exception as exp:
-                printRed(exp)
+                printDarkRed("[ERROR] [UNAUTH] save log to file")
+                printDarkRed(exp)
