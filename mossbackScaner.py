61c82a37a0c945f441882c3a03d331bd2210a1c8
Kafroc <kafrocyang@gmail.com>
2020-12-09 14:16:16 +0800

    Update mossbackScaner.py

--- mossbackScaner.py
+++ mossbackScaner.py
@@ -1,64 +1,52 @@
 # -*- coding: utf-8 -*-
-# version: 0.2
+# version: 0.5
 # date: 2020.09.22
 
 import json
-import re
 import socket
-import sys
-import http.client
 import time
 import threading
-from socket import *
-from time import ctime
-import hashlib
-import subprocess
+import socket
 from subprocess import PIPE
 
 from color_print import *
-
-from scanner_sqli import test_sqli
-from scanner_cmdi import test_cmdi
-from scanner_unauth_access import test_unauth_access
+from utils import format_save
 
 global glo_conf
 global glo_pkg_list
 global glo_lock
 global glo_scanner
 
-
-def scanner_factory(name):
-    return eval(name + "()")
-
+glo_pkg_list = []
+glo_lock = threading.Lock()
+glo_scanner = []
 
 with open('config.json', 'r') as fp:
     glo_conf = json.loads(fp.read())
 
-glo_pkg_list = []
-
-glo_lock = threading.Lock()
-
-glo_scanner = []
-glo_scanner.append(scanner_factory("test_sqli"))
-glo_scanner.append(scanner_factory("test_cmdi"))
-glo_scanner.append(scanner_factory("test_unauth_access"))
-
+for plugin in glo_conf['plugins']:
+    exec("from " + plugin + " import " + plugin)
+    glo_scanner.append(eval(plugin + "()"))
 
 def do_scan_thread():
     global glo_pkg_list
     global glo_lock
     global glo_scanner
 
+    req2file = format_save('')
     while True:
         glo_lock.acquire()
         if len(glo_pkg_list) > 0:
             pkg = json.loads(glo_pkg_list.pop(0))
             glo_lock.release()
 
+            
+            if glo_conf['server_host'] != '*':
+                req2file.save_request(pkg['method'], pkg['uri'], pkg['version'], pkg['header'], pkg['body'])
+            
             # do all test here
             for fun in glo_scanner:
-                fun.run(pkg['method'], pkg['uri'], pkg['version'],
-                        pkg['header'], pkg['body'], pkg['host'])
+                fun.run(pkg['method'], pkg['uri'], pkg['version'], pkg['header'], pkg['body'])
             # test finished
         else:
             glo_lock.release()
@@ -73,34 +61,29 @@ def main():
     t = threading.Thread(target=do_scan_thread, args=())
     t.start()
 
-    BUFSIZ = 1024
+    MAXBUFSIZ = 1048576
     ADDRESS = ('', glo_conf['scanner_port'])
-    udpServerSocket = socket(AF_INET, SOCK_DGRAM)
-    udpServerSocket.bind(ADDRESS)
+    tcpServerSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcpServerSocket.bind(ADDRESS)
+    tcpServerSocket.listen()
+    mitmCli = None
 
     while True:
-        # pack_meta = pack_cnt(4 B) + pack_hash(32 B)
-        pack_meta, pack_from = udpServerSocket.recvfrom(36)
-        pack_meta = pack_meta.decode()
-        pack_data = ''
-        for i in range(int(pack_meta[:4])):
-            data, _ = udpServerSocket.recvfrom(BUFSIZ)
-            pack_data += data.decode()
+        while mitmCli is None:
+            try:
+                mitmCli, _ = tcpServerSocket.accept()
+            except:
+                time.sleep(0.1)
 
-        m = hashlib.md5()
-        m.update(pack_data.encode())
-
-        if pack_meta[4:] != m.hexdigest():
-            printDarkRed("[ERROR] the hash of received message incorrect.")
-            udpServerSocket.sendto("ER".encode('utf-8'), pack_from)
-        else:
-            udpServerSocket.sendto("OK".encode('utf-8'), pack_from)
+        
+        try:
+            pack_data = mitmCli.recv(MAXBUFSIZ)
+        except:
+            mitmCli = None
 
         glo_lock.acquire()
         glo_pkg_list.append(pack_data)
         glo_lock.release()
-
-    udpServerSocket.close()
 
 
 if __name__ == "__main__":
